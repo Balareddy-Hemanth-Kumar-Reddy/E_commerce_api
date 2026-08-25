05706f7d4dd86a48dbdff3faa43260bdef283875
Hemanth Kumar <hemanthkumar200421@gmail.com>
2026-08-25 11:16:00 +0530

    all apis created and working

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,15 @@
 from django.urls import path
-from .views import hello
+
+from .views import hello, products, product_detail
+
 
 urlpatterns = [
     path("hello/", hello, name="hello"),
+
+    path("products/", products, name="products"),
+    path(
+        "products/<int:pk>/",
+        product_detail,
+        name="product-detail"
+    ),
 ]
--- api/views.py
+++ api/views.py
@@ -1,5 +1,9 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework import status
+
+from .models import Product
+from .serializers import ProductSerializer
 
 
 @api_view(["GET"])
@@ -7,3 +11,70 @@ def hello(request):
     return Response({
         "message": "E-Commerce API is working!"
     })
+
+
+@api_view(["GET", "POST"])
+def products(request):
+
+    if request.method == "GET":
+        products = Product.objects.all()
+        serializer = ProductSerializer(products, many=True)
+
+        return Response(serializer.data)
+
+    if request.method == "POST":
+        serializer = ProductSerializer(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+
+            return Response(
+                serializer.data,
+                status=status.HTTP_201_CREATED
+            )
+
+        return Response(
+            serializer.errors,
+            status=status.HTTP_400_BAD_REQUEST
+        )
+
+
+@api_view(["GET", "PUT", "DELETE"])
+def product_detail(request, pk):
+
+    try:
+        product = Product.objects.get(pk=pk)
+
+    except Product.DoesNotExist:
+        return Response(
+            {"error": "Product not found"},
+            status=status.HTTP_404_NOT_FOUND
+        )
+
+    if request.method == "GET":
+        serializer = ProductSerializer(product)
+
+        return Response(serializer.data)
+
+    if request.method == "PUT":
+        serializer = ProductSerializer(
+            product,
+            data=request.data
+        )
+
+        if serializer.is_valid():
+            serializer.save()
+
+            return Response(serializer.data)
+
+        return Response(
+            serializer.errors,
+            status=status.HTTP_400_BAD_REQUEST
+        )
+
+    if request.method == "DELETE":
+        product.delete()
+
+        return Response(
+            {"message": "Product deleted successfully"}
+        )
